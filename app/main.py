85cab30ddebab22725846f7d746ae4e3acf33d70
baucorporation <baucorpn@gmail.com>
2025-07-16 12:45:51 +0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,52 +1,64 @@
 import sys
 import os
 import zlib
-import hashlib
+from hashlib import sha1
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
 
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
+            f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
-    elif command == "cat-file" and sys.argv[2] == "-p":
-        file = sys.argv[3]
-        filename = f".git/objects/{file[0:2]}/{file[2:]}"
-        with open(filename, "rb") as f:
-            data = f.read()
-            data = zlib.decompress(data)
-            header_end = data.find(b"\x00")
-            content = data[header_end + 1 :].strip()
-            print(content.decode("utf-8"), end="")
-
+    elif command == "cat-file":
+        if not sys.argv[2] == "-p":
+            raise RuntimeError(f"Unexpected flag #{sys.argv[2]}")
+        dname, fname = sys.argv[3][:2], sys.argv[3][2:]
+        with open(os.path.join(".git/objects", dname, fname), "rb") as f:
+            _, output = zlib.decompress(f.read()).split(b"\x00")
+        sys.stdout.buffer.write(output)
     elif command == "hash-object":
-        if sys.argv[2] == "-w":
-            filename = sys.argv[3]
-        else:
-            filename = sys.argv[2]
-        # get file content bin
-        with open(filename, "rb") as f:
-            data = f.read()
-        out_data = f"blob {len(data)}\0".encode("utf-8") + data
-        hash = hashlib.sha1(out_data).hexdigest()
-        new_dir = hash[0:2]
-        new_path = f".git/objects/{new_dir}/{hash[2:]}"
-        out_data_compressed = zlib.compress(out_data)
-        if sys.argv[2] == "-w":
-            if not os.path.exists(f".git/objects/{hash[0:2]}"):
-                os.mkdir(f".git/objects/{hash[0:2]}")
-            with open(new_path, "wb") as g:
-                g.write(out_data_compressed)
+        if not sys.argv[2] == "-w":
+            raise RuntimeError(f"Unexpected flag #{sys.argv[2]}")
+
+        with open(sys.argv[3], "rb") as f:
+            contents = f.read()
+
+        header = f"blob {len(contents)}\x00".encode("utf-8")
+        hash = sha1(header + contents).hexdigest()
         print(hash)
+        dname, fname = hash[:2], hash[2:]
+        dname = os.path.join(".git/objects", dname)
+        os.mkdir(dname)
+        with open(os.path.join(dname, fname), "wb") as f:
+            f.write(zlib.compress(header + contents))
+
+    elif command == "ls-tree":
+        if not sys.argv[2] == "--name-only":
+            raise RuntimeError(f"Unexpected flag #{sys.argv[2]}")
+        dname, fname = sys.argv[3][:2], sys.argv[3][2:]
+        with open(os.path.join(".git/objects", dname, fname), "rb") as f:
+            _, treedata = zlib.decompress(f.read()).split(b"\x00", 1)
+            res = []
+            while treedata:
+                # we don't need the mode
+                _, treedata = treedata.split(b" ", 1)
+                entry, treedata = treedata.split(b"\x00", 1)
+                res.append(entry)
+                treedata = treedata[20:]
+            print("\n".join(res))
+
+    elif command == "write-tree":
+        res = b""
+        entries = os.scandir()
+        for entry in entries:
+            mode = f"{entry.stat().st_mode:o}"
+
+        raise RuntimeError("write-tree not implemented yet")
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
