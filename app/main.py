5a7d02ca8561d6ca6a5556d0dc469c7e1ee51c71
baucorporation <baucorpn@gmail.com>
2025-07-16 12:42:55 +0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import zlib
+import hashlib
 
 
 def main():
@@ -16,6 +18,36 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file" and sys.argv[2] == "-p":
+        file = sys.argv[3]
+        filename = f".git/objects/{file[0:2]}/{file[2:]}"
+        with open(filename, "rb") as f:
+            data = f.read()
+            data = zlib.decompress(data)
+            header_end = data.find(b"\x00")
+            content = data[header_end + 1 :].strip()
+            print(content.decode("utf-8"), end="")
+
+    elif command == "hash-object":
+        if sys.argv[2] == "-w":
+            filename = sys.argv[3]
+        else:
+            filename = sys.argv[2]
+        # get file content bin
+        with open(filename, "rb") as f:
+            data = f.read()
+        out_data = f"blob {len(data)}\0".encode("utf-8") + data
+        hash = hashlib.sha1(out_data).hexdigest()
+        new_dir = hash[0:2]
+        new_path = f".git/objects/{new_dir}/{hash[2:]}"
+        out_data_compressed = zlib.compress(out_data)
+        if sys.argv[2] == "-w":
+            if not os.path.exists(f".git/objects/{hash[0:2]}"):
+                os.mkdir(f".git/objects/{hash[0:2]}")
+            with open(new_path, "wb") as g:
+                g.write(out_data_compressed)
+        print(hash)
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
